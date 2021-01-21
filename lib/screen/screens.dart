3f897d91285dad3d287ff49237153f7a42893197
AKid <gaopengfeidev@gmail.com>
2021-01-21 12:11:16 +0800

    :fire: 移除cupertino.dart无用引用，重命名登录页面相关命名

--- lib/screen/screens.dart
+++ lib/screen/screens.dart
@@ -1,6 +1,6 @@
 export './module_main/home.dart';
 export './module_main/splash.dart';
-export './module_main/sign_up_sign_in.dart';
+export './module_main/login.dart';
 export './module_dashboard/dashboard_page.dart';
 export './module_dashboard/balance.dart';
 export './module_dashboard/withdraw.dart';
