439dbe9e96a60341a9b4624536dc521e4b92c88a
wesley.wang <9610848@qq.com>
2021-03-04 11:02:04 +0800

    fix video player

--- lib/widgets/loading.dart
+++ lib/widgets/loading.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:idol/res/colors.dart';
 
-class IdolLoadingWidget extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _IdolLoadingWidgetState();
-}
-
-class _IdolLoadingWidgetState extends State<IdolLoadingWidget> {
+class IdolLoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print('IdolLoading Widget buid');
     return Center(
       child: Container(
-        width: 24,
-        height: 24,
-        child: CircularProgressIndicator(
-          strokeWidth: 3,
-          valueColor: AlwaysStoppedAnimation<Color>(Colours.color_EA5228),
-        )
-      ),
+          width: 24,
+          height: 24,
+          child: CircularProgressIndicator(
+            strokeWidth: 3,
+            valueColor: AlwaysStoppedAnimation<Color>(Colours.color_EA5228),
+          )),
     );
   }
 }
