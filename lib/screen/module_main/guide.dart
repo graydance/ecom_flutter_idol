9e142541f9ba8c62b93877500f6730b522d36ac3
bobingxin <bobingxin@qq.com>
2021-03-23 17:05:14 +0800

    添加引导

--- lib/screen/module_main/guide.dart
+++ lib/screen/module_main/guide.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/html_parser.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:idol/r.g.dart';
 import 'package:idol/res/colors.dart';
@@ -24,89 +26,131 @@ class _GuideScreenState extends State<GuideScreen> {
     'We can guarantee a much higher revenue compared with others.',
   ];
 
+  static const List<String> guideClick = [
+    'Next page',
+    'Next page',
+    'Land',
+  ];
+
+  SwiperController controllrer = SwiperController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: R.image.launch_background_webp(), fit: BoxFit.cover),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                height: 330,
-                padding:
-                    EdgeInsets.only(top: 70, left: 16, right: 16, bottom: 40),
-                color: Colours.color_white20,
-                child: Swiper(
-                  itemCount: guideTitle.length,
-                  autoplay: false,
-                  viewportFraction: 1,
-                  loop: false,
-                  scale: 1,
-                  onIndexChanged: (index) {
-                    setState(() {
-                      _showGetStarted = index == guideTitle.length - 1;
-                    });
-                  },
-                  itemBuilder: (context, index) {
-                    return _createSwiperItem(index);
-                  },
-                  pagination: SwiperPagination(
-                    alignment: Alignment.bottomCenter,
-                    builder: DotSwiperPaginationBuilder(
-                      // 点之间的间隔
-                        space: 2,
-                        // 没选中时的大小
-                        size: 6,
-                        // 选中时的大小
-                        activeSize: 8,
-                        // 没选中时的颜色
-                        color: Colours.color_black45,
-                        //选中时的颜色
-                        activeColor: Colors.white),
-                  ),
-                ),
-              ),
-              Visibility(
-                maintainState: true,
-                maintainAnimation: true,
-                maintainSize: true,
-                child: GestureDetector(
-                  onTap: () {
-                    IdolRoute.startValidateEmail(context);
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colours.color_white30,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(4),
-                      ),
-                    ),
-                    padding: EdgeInsets.only(
-                        left: 30, top: 12, right: 30, bottom: 12),
-                    margin: EdgeInsets.only(left: 90, right: 90, top: 50),
-                    child: Text(
-                      'GET STARTED!',
-                      style:
-                          TextStyle(fontSize: 18, color: Colours.color_FFFFF0),
+        body: Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: R.image.launch_background_webp(), fit: BoxFit.cover),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 350,
+                  padding:
+                      EdgeInsets.only(top: 0, left: 0, right: 0, bottom: 30),
+                  color: Colours.color_white20,
+                  child: Swiper(
+                    itemCount: guideTitle.length,
+                    autoplay: false,
+                    viewportFraction: 1,
+                    loop: false,
+                    outer: true,
+                    scale: 1,
+                    controller: controllrer,
+                    onIndexChanged: (index) {
+                      setState(() {
+                        _showGetStarted = index == guideTitle.length - 1;
+                      });
+                    },
+                    itemBuilder: (context, index) {
+                      return _createSwiperItem(index, context, controllrer);
+                    },
+                    pagination: SwiperPagination(
+                      alignment: Alignment.bottomCenter,
+                      builder: RoundRectSwiperPaginationBuilder(
+                          //点之间的间隔
+                          space: 5,
+                          // 没选中时的大小
+                          size: Size(28, 3),
+                          // 选中时的大小
+                          activeSize: Size(28, 3),
+                          // 没选中时的颜色
+                          color: Colours.color_black45,
+                          //选中时的颜色
+                          activeColor: Colors.white),
                     ),
                   ),
                 ),
-                visible: _showGetStarted,
-              ),
-            ],
+                // Container(
+                //   decoration: BoxDecoration(
+                //     color: Colours.color_white30,
+                //     borderRadius: BorderRadius.all(
+                //       Radius.circular(4),
+                //     ),
+                //   ),
+                //   padding:
+                //       EdgeInsets.only(left: 30, top: 12, right: 30, bottom: 12),
+                //   margin: EdgeInsets.only(left: 90, right: 90, top: 50),
+                //   child: Text(
+                //     'GET STARTED!',
+                //     style: TextStyle(fontSize: 18, color: Colours.color_FFFFF0),
+                //   ),
+                // ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+        Column(
+          children: [
+            Spacer(),
+            Center(
+              child: Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 36),
+                child: Column(
+                  children: [
+                    Text(
+                      "By signing up, you also agree with our ",
+                      style:
+                          TextStyle(color: Colours.color_636365, fontSize: 12),
+                    ),
+                    RichText(
+                        text: TextSpan(children: [
+                      TextSpan(
+                          text: "Terms of Use",
+                          style: TextStyle(
+                              color: Colours.color_4E9AE3, fontSize: 12),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => print('click1')),
+                      TextSpan(
+                        text: " and ",
+                        style: TextStyle(
+                            color: Colours.color_636365, fontSize: 12),
+                      ),
+                      TextSpan(
+                          text: "Privacy Policy",
+                          style: TextStyle(
+                              color: Colours.color_4E9AE3, fontSize: 12),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => print('click2'))
+                    ]))
+                  ],
+                ),
+              ),
+            )
+          ],
+        )
+      ],
+    ));
   }
 
-  Widget _createSwiperItem(int index) {
+  Widget _createSwiperItem(
+      int index, BuildContext context, SwiperController controllrer) {
     return Container(
+      margin: EdgeInsets.fromLTRB(16, 64, 16, 0),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -125,13 +169,111 @@ class _GuideScreenState extends State<GuideScreen> {
           Text(
             guideDesc[index],
             textAlign: TextAlign.center,
-            style: TextStyle(color: Colours.color_white70, fontSize: 16),
+            style: TextStyle(color: Colours.color_white70, fontSize: 14),
           ),
-          SizedBox(
-            height: 70,
+          GestureDetector(
+            onTap: () {
+              if (index == 2)
+                IdolRoute.startValidateEmail(context);
+              else
+                controllrer.next(animation: true);
+            },
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 35, 0, 0),
+              //设置 child 居中
+              alignment: Alignment(0, 0),
+              height: 22,
+              width: 76,
+              //边框设置
+              decoration: new BoxDecoration(
+                //背景
+                color: Colors.transparent,
+                //设置四周圆角 角度 这里的角度应该为 父Container height 的一半
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                //设置四周边框
+                border: new Border.all(width: 1, color: Colors.white),
+              ),
+              child: Text(guideClick[index],
+                  textAlign: TextAlign.center,
+                  style: new TextStyle(color: Colors.white, fontSize: 12)),
+            ),
           ),
         ],
       ),
     );
+  }
+}
+
+class RoundRectSwiperPaginationBuilder extends SwiperPlugin {
+  final Color activeColor;
+
+  ///,if set null , will be Theme.of(context).scaffoldBackgroundColor
+  final Color color;
+
+  ///Size of the rect when activate
+  final Size activeSize;
+
+  ///Size of the rect
+  final Size size;
+
+  /// Space between rects
+  final double space;
+
+  final Key key;
+
+  const RoundRectSwiperPaginationBuilder(
+      {this.activeColor,
+      this.color,
+      this.key,
+      this.size: const Size(10.0, 2.0),
+      this.activeSize: const Size(10.0, 2.0),
+      this.space: 3.0});
+
+  @override
+  Widget build(BuildContext context, SwiperPluginConfig config) {
+    ThemeData themeData = Theme.of(context);
+    Color activeColor = this.activeColor ?? themeData.primaryColor;
+    Color color = this.color ?? themeData.scaffoldBackgroundColor;
+
+    List<Widget> list = [];
+
+    if (config.itemCount > 20) {
+      print(
+          "The itemCount is too big, we suggest use FractionPaginationBuilder instead of DotSwiperPaginationBuilder in this sitituation");
+    }
+
+    int itemCount = config.itemCount;
+    int activeIndex = config.activeIndex;
+
+    for (int i = 0; i < itemCount; ++i) {
+      bool active = i == activeIndex;
+      Size size = active ? this.activeSize : this.size;
+      list.add(Container(
+        margin: EdgeInsets.fromLTRB(space, 0, space, 0),
+        width: size.width,
+        height: size.height,
+        //边框设置
+        decoration: new BoxDecoration(
+          //背景
+          color: active ? activeColor : color,
+          //设置四周圆角 角度
+          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+        ),
+      ));
+    }
+
+    if (config.scrollDirection == Axis.vertical) {
+      return new Column(
+        key: key,
+        mainAxisSize: MainAxisSize.min,
+        children: list,
+      );
+    } else {
+      return new Row(
+        key: key,
+        mainAxisSize: MainAxisSize.min,
+        children: list,
+      );
+    }
   }
 }
